756a728d2c54d7b3425af6a19e57997fe0258fe1
paul kabuu <kongkabuu9@gmail.com>
2023-06-08 09:49:36 +0300

    installed browser router

--- ink_project/src/components/Footer.jsx
+++ ink_project/src/components/Footer.jsx
@@ -1,10 +1,8 @@
 function Footer() {
   return (
-    <footer className="bg-gray-200 py-4">
+    <footer className="bg-gray-200 py-4 bottom-0">
       <div className="container mx-auto text-center">
-        <p className="text-gray-600">
-          © 2023 Your Website. All rights reserved.
-        </p>
+        <p className="text-gray-600">© 2023 ink.com . All rights reserved.</p>
       </div>
     </footer>
   );
