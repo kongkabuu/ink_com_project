77b81c4373690fda5c3c15cc2b716a3212153b01
paul kabuu <kongkabuu9@gmail.com>
2023-06-08 09:27:46 +0300

    added footer to app component

--- ink_project/src/App.jsx
+++ ink_project/src/App.jsx
@@ -1,13 +1,14 @@
 import Header from "./components/Header";
 import Articlelist from "./components/Articlelist";
-
 import "./index.css";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div>
       <Header />
       <Articlelist />
+      <Footer />
     </div>
   );
 }
---
+++ ink_project/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+function Footer() {
+  return (
+    <footer className="bg-gray-200 py-4">
+      <div className="container mx-auto text-center">
+        <p className="text-gray-600">
+          © 2023 Your Website. All rights reserved.
+        </p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
